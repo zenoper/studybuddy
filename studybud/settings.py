ab64bb500fd0995d4098ed956d366a8b58ac8b4f
Muhammad <82376665+zenoper@users.noreply.github.com>
2023-01-15 23:33:08 +0500

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i^in9xj%-@phgq!+ied9r=%l2hwl55kq%24pejc!**n53@zrb!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
